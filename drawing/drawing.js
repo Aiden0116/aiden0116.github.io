be1b4edb77dfaa472f4f0172e6c43eb896a89307
Aiden0116 <aidenchoi0116@gmail.com>
2018-11-13 09:40:05 -0800

    changed css background

---
+++ drawing/drawing.js
@@ -0,0 +1,25 @@
+// Variable
+// var x = 0;
+// x = 1;
+
+// constant
+// const y = 0;
+// y = 1;
+
+var canvas;
+var ctx;
+
+var unit = 10;
+window.onload = init;
+
+function init()  {
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    // Filling a rectangle
+    ctx.fillRect(0,0,100,100);
+    ctx.fillstyle = "black";
+    ctx.fillRect(0,0,100,100);
+drawRuler(10); 
+ctx.fillRect(60*unit,10*unit,10*unit,10*unit)
+
+}
