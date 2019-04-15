6ad06155ea0844b7d1990e86c4ce307a5c603d2d
Aiden Choi <hyunjung@MIDGET-CAMMER.local>
2019-04-15 10:09:19 -0700

    walk

--- drawing/drawCloud.js
+++ drawing/drawCloud.js
@@ -1,14 +1,13 @@
-function drawCloud(x,y,scale){
+function drawCloud(x,y){
     ctx.save();
     ctx.translate(x*UNIT,y*UNIT);
-    ctx.scale(scale,scale);
+    ctx.scale(5,5);
     ctx.fillStyle= "white";
     ctx.fillRect(UNIT,0,3*UNIT,UNIT);
     ctx.fillRect(0,UNIT,5*UNIT,UNIT);
     ctx.fillRect(UNIT,2*UNIT,3*UNIT,UNIT);
     ctx.restore();
-    ctx.restore();
 
    
-    
+
 }
